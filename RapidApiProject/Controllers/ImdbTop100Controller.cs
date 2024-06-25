30205b0f0ca59a2462d056901afdb11cd6b43f2c
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-06-25 20:28:05 +0300

    Add project files.

---
+++ RapidApiProject/Controllers/ImdbTop100Controller.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RapidApiProject.Controllers;
+public class ImdbTop100Controller : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
