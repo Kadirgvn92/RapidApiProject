bba1502488ae5368244c1a09fdc00c3be25fefe4
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-06-26 22:10:29 +0300

    Weather api get with ajax

--- RapidApiProject/Controllers/DefaultController.cs
+++ RapidApiProject/Controllers/DefaultController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiProject.Models;
+using RestSharp;
 
 namespace RapidApiProject.Controllers;
 public class DefaultController : Controller
 {
+
     public IActionResult Index()
     {
         return View();
     }
+    [HttpGet]
+    public IActionResult GetWeather()
+    {
+        var client = new RestClient("https://api.collectapi.com/weather/getWeather?data.lang=tr&data.city=ankara");
+        var request = new RestRequest(Method.GET);
+        request.AddHeader("authorization", "apikey 3vLvr8BLRuJZH5KJDssMMM:3s0zddCcs13UHM3tSE1TPp");
+        request.AddHeader("content-type", "application/json");
+        IRestResponse response = client.Execute(request);
 
+        if (response.IsSuccessful)
+        {
+            var weatherApiResponse = JsonConvert.DeserializeObject<WeatherApiResponse>(response.Content);
+            return Ok(weatherApiResponse); // API'den doğru şekilde veri alındığında 200 OK yanıtıyla veriyi döndür
+        }
 
+        return BadRequest("Hava durumu bilgisi alınamadı.");
+    }
+    
 }
+
--- RapidApiProject/Models/WeatherApiResponse.cs
+++ RapidApiProject/Models/WeatherApiResponse.cs
@@ -2,14 +2,22 @@
 
 public class WeatherApiResponse
 {
-    public string date { get; set; }
-    public string day { get; set; }
-    public string icon { get; set; }
-    public string description { get; set; }
-    public string status { get; set; }
-    public string degree { get; set; }
-    public string min { get; set; }
-    public string max { get; set; }
-    public string night { get; set; }
-    public string humidity { get; set; }
+    public bool success { get; set; }
+    public string city { get; set; }
+    public List<WeatherData> result { get; set; }
+
+
+    public class WeatherData
+    {
+        public string date { get; set; }
+        public string day { get; set; }
+        public string icon { get; set; }
+        public string description { get; set; }
+        public string status { get; set; }
+        public string degree { get; set; }
+        public string min { get; set; }
+        public string max { get; set; }
+        public string night { get; set; }
+        public string humidity { get; set; }
+    }
 }
