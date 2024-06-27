e0cbe1c976434a8cadff50b7320620ef741a338c
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-06-28 01:17:22 +0300

    Book api cannot consumed will have checked later

---
+++ RapidApiProject/Models/BookViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace RapidApiProject.Models;
+
+public class BookViewModel
+{
+    public bool success { get; set; }
+    public List<BookData> result { get; set; }
+}
+public class BookData
+{
+    public string url { get; set; }
+    public string indirim { get; set; }
+    public string fiyat { get; set; }
+    public string yayın { get; set; }
+    public string yazar { get; set; }
+    public string title { get; set; }
+    public string image { get; set; }
+
+}
--- RapidApiProject/ViewComponents/_PopularNewsPartial.cs
+++ RapidApiProject/ViewComponents/_PopularNewsPartial.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiProject.Models;
+using RestSharp;
 
 namespace RapidApiProject.ViewComponents;
 
@@ -6,6 +9,17 @@ public class _PopularNewsPartial : ViewComponent
 {
     public IViewComponentResult Invoke()
     {
+        var client = new RestClient("https://api.collectapi.com/book/newBook");
+        var request = new RestRequest(Method.GET);
+        request.AddHeader("authorization", "apikey 3vLvr8BLRuJZH5KJDssMMM:3s0zddCcs13UHM3tSE1TPp");
+        request.AddHeader("content-type", "application/json");
+        IRestResponse response = client.Execute(request);
+
+        if (response.IsSuccessful)
+        {
+            var data = JsonConvert.DeserializeObject<BookViewModel>(response.Content);
+            return View(data);  
+        }
         return View();
     }
 }
