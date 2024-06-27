51ad2522b1ded97f3cb65a8c4012c8b754ab828b
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-06-28 00:58:06 +0300

    Namaz vakitleri api consumed

---
+++ RapidApiProject/Models/FaizViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace RapidApiProject.Models;
+
+public class NamazViewModel
+{
+    public bool success { get; set; }
+    public List<NamazData> result { get; set; }
+
+
+    public class NamazData
+    {
+        public string saat { get; set; }
+        public string vakit { get; set; }
+
+    }
+}
--- RapidApiProject/ViewComponents/_LatestNewsPartial.cs
+++ RapidApiProject/ViewComponents/_LatestNewsPartial.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiProject.Models;
+using RestSharp;
 
 namespace RapidApiProject.ViewComponents;
 
@@ -6,6 +9,16 @@ public class _LatestNewsPartial : ViewComponent
 {
     public IViewComponentResult Invoke()
     {
+        var client = new RestClient("https://api.collectapi.com/pray/all?data.city=istanbul");
+        var request = new RestRequest(Method.GET);
+        request.AddHeader("authorization", "apikey 3vLvr8BLRuJZH5KJDssMMM:3s0zddCcs13UHM3tSE1TPp");
+        request.AddHeader("content-type", "application/json");
+        IRestResponse response = client.Execute(request);
+        if (response.IsSuccessful)
+        {
+            var data = JsonConvert.DeserializeObject<NamazViewModel>(response.Content);
+            return View(data);
+        }
         return View();
     }
 }
