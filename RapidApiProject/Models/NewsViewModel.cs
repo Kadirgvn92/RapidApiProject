8ac7b63ddf3dc79c9f5c7e103e6290198edcb743
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-06-28 00:41:09 +0300

    News api consumed in MainPost ViewComponent

---
+++ RapidApiProject/Models/NewsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using static RapidApiProject.Models.WeatherApiResponse;
+
+namespace RapidApiProject.Models;
+
+public class NewsViewModel
+{
+    public bool success { get; set; }
+    public List<NewsData> result { get; set; }
+}
+
+public class NewsData
+{
+    public string key { get; set; }
+    public string url { get; set; }
+    public string description { get; set; }
+    public string image { get; set; }
+    public string name { get; set; }
+    public string source { get; set; }
+}
--- RapidApiProject/ViewComponents/_MainPostPartial.cs
+++ RapidApiProject/ViewComponents/_MainPostPartial.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiProject.Models;
+using RestSharp;
 
 namespace RapidApiProject.ViewComponents;
 
@@ -6,7 +9,17 @@ public class _MainPostPartial : ViewComponent
 {
     public IViewComponentResult Invoke()
     {
+        var client = new RestClient("https://api.collectapi.com/news/getNews?country=tr&tag=general");
+        var request = new RestRequest(Method.GET);
+        request.AddHeader("authorization", "apikey 3vLvr8BLRuJZH5KJDssMMM:3s0zddCcs13UHM3tSE1TPp");
+        request.AddHeader("content-type", "application/json");
+        IRestResponse response = client.Execute(request);
 
+        if (response.IsSuccessful)
+        {
+            var apiResponse = JsonConvert.DeserializeObject<NewsViewModel>(response.Content);
+            return View(apiResponse);
+        }
         return View();
     }
 }
