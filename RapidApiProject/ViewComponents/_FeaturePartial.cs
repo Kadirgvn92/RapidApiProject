ddc0914229e60ed4533f2a2353bb46fc40560ddc
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-06-28 00:16:28 +0300

    Feature designed with api

--- RapidApiProject/ViewComponents/_FeaturePartial.cs
+++ RapidApiProject/ViewComponents/_FeaturePartial.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiProject.Models;
+using RestSharp;
 
 namespace RapidApiProject.ViewComponents;
 
@@ -6,6 +9,17 @@ public class _FeaturePartial : ViewComponent
 {
     public IViewComponentResult Invoke()
     {
+        var client = new RestClient("https://imdb-top-100-movies.p.rapidapi.com/");
+        var request = new RestRequest(Method.GET);
+        request.AddHeader("x-rapidapi-key", "1b835557bamsh51f42a1ec11b465p131830jsn7e5d47c9227c");
+        request.AddHeader("x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com");
+        IRestResponse response = client.Execute(request);
+
+        if (response.IsSuccessful)
+        {
+            var apiResponse = JsonConvert.DeserializeObject<List<NavbarMovieViewModel>>(response.Content);
+            return View(apiResponse);
+        }
         return View();
     }
 }
